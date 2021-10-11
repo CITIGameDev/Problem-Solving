9df04949fc5d065a08af9a1553f833c390cdad6e
erzhanto <erzhariessfeld@gmail.com>
2021-10-11 19:13:50 +0700

    Problem 4

---
+++ Assets/Script/BallMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float Kecepatan;
+    Rigidbody2D ballbody;
+    private void Awake()
+    {
+        ballbody = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        ballbody.velocity = new Vector2(Kecepatan * horizontal, ballbody.velocity.y);
+        ballbody.velocity = new Vector2(ballbody.velocity.x, Kecepatan * vertical);
+    }
+}
