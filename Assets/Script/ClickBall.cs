f04110af01b41c7cd2ae0730294895c5ed47f5d6
erzhanto <erzhariessfeld@gmail.com>
2021-10-11 19:15:58 +0700

    Problem 5

---
+++ Assets/Script/ClickBall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickBall : MonoBehaviour
+{
+    private Rigidbody2D ballbody;
+    public float kecepatan;
+    public Camera cams;
+    Vector3 curs;
+    Vector3 angl;
+    private void Start()
+    {
+        ballbody = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        curs = cams.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+        angl = curs - transform.position;
+        if (Vector3.Distance(curs, transform.position) > 0.3)
+        {
+            transform.Translate(angl.normalized * kecepatan * Time.deltaTime);
+        }
+
+
+    }
+
+
+}
