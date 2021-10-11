2296cf5cd71c6fb98df3a291e3db9cb65e9f1c26
erzhanto <erzhariessfeld@gmail.com>
2021-10-11 19:18:06 +0700

    Problem 6

---
+++ Assets/Script/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+	public Transform[] spawn;
+	public bool isspawn = true;
+	public GameObject squares;
+	public Transform titik;
+	void Update()
+	{
+		titik = spawn[Random.Range(0, spawn.Length)];
+		if (isspawn == true)
+		{
+			Instantiate(squares, titik);
+			isspawn = false;
+			StartCoroutine(ToSpawnTrue());
+		}
+	}
+	IEnumerator ToSpawnTrue()
+	{
+		yield return new WaitForSeconds(2);
+		isspawn = true;
+	}
+}
